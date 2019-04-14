d310136c7ef06fb51592241ccd1782127ddb8ba3
codegenesis <shashijsr@gmail.com>
2019-04-14 13:05:37 -0400

    added few more examples

---
+++ src/main/scala/practisescala/Practise.scala
@@ -0,0 +1,30 @@
+package practisescala
+
+import java.lang
+
+/**
+  * created by skant
+  */
+object Practise extends App {
+
+
+
+//  override def main(args: Array[String]): Unit = {
+//
+//    val fruits = Seq("Appl", "Ora", "Bana")
+//    println(fruits)
+//
+//    //fruits.map(_.toUpperCase)
+//    println(fruits.map((s: String) => s.toUpperCase))
+//
+//
+//    println(fruits.flatMap((s: String) => s.toUpperCase))
+//  }
+
+  override def main(args: String): Unit =
+
+    //val string = "sha"
+    println(" " +args)
+  
+
+}
--- src/main/scala/progscala2/introscala/upper1.scala
+++ src/main/scala/progscala2/introscala/upper1.scala
@@ -4,6 +4,6 @@ package progscala2.introscala
 object Upper {
   def main(args: Array[String]) = {
     args.map(_.toUpperCase()).foreach(printf("%s ",_))
-    println("")
+    println("---")
   }
 }
